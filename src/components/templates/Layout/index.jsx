37bf0206cb235cbe8de764865efbfa2f19c17683
Hello-ImNew <baokhanhtk129@gmail.com>
2025-08-13 15:26:41 -0400

    add spacer

--- src/components/templates/Layout/index.jsx
+++ src/components/templates/Layout/index.jsx
@@ -24,6 +24,7 @@ function Layout(props) {
                 </div>
             </div>
             <div>
+                <br/>
                 <div>
                     {props.children}
                 </div>
